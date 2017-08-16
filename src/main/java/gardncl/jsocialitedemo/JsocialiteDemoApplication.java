5e4470c790a8281bf7872d1bfecd6ad68c48373c
gardncl <gardncl@gmail.com>
2017-08-16 19:18:33 -0400

    first commit from spring initializer

---
+++ src/main/java/gardncl/jsocialitedemo/JsocialiteDemoApplication.java
@@ -0,0 +1,12 @@
+package gardncl.jsocialitedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsocialiteDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsocialiteDemoApplication.class, args);
+	}
+}
